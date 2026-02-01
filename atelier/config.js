8bd31e2045feba9956321865434ebd971256a1a9
damien pichard <magvl4gleize@durandservices.fr>
2026-02-01 10:59:33 +0100

    Add files via upload

---
+++ atelier/config.js
@@ -0,0 +1,10 @@
+window.__ATELIER_CFG = {
+  // URL de ton Apps Script (celle qui renvoie lignes/regles)
+  GS_URL: "https://script.google.com/macros/s/TON_ID/exec",
+
+  // (optionnel) mots de passe suivi si tu veux les gérer ici aussi
+  ATELIER_SUIVI_PASS_STE: "",
+  ATELIER_SUIVI_PASS_BG: "",
+  ATELIER_SUIVI_PASS_LIMITED: "",
+  ATELIER_SUIVI_PASS_CHASSE: ""
+};
